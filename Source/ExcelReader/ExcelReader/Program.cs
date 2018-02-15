0fa1cdd3ab806733d9ad8578d53c6db50622e879
petervais <peter-vais@hotmail.sk>
2018-02-15 15:29:00 +0100

    AddedReadingExcelFile

--- Source/ExcelReader/ExcelReader/Program.cs
+++ Source/ExcelReader/ExcelReader/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Excel;
 
 namespace ExcelReader
 {
@@ -10,6 +14,42 @@ namespace ExcelReader
     {
         static void Main(string[] args)
         {
+            //Create COM Objects. Create a COM object for everything that is referenced
+            Application xlApp = new Application();
+            Workbook xlWorkbook = xlApp.Workbooks.Open(@"D:\DIPdoc\RuleEngine\Occupationslist - no rules.xlsx");
+            Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+
+            //iterate over the rows and columns and print to the console as it appears in the file
+            //excel is not zero based!!!
+            for (int i = 3; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
+                {
+                    //write the value to the console
+                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                        Console.WriteLine(xlRange.Cells[i, j].Value2.ToString());
+                }
+            }
+
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
         }
     }
 }
